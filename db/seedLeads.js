06d708a264905ebb07da5d9e1b68b5ed830530d4
bccbass <109984363+bccbass@users.noreply.github.com>
2024-12-01 17:21:52 +1100

    created lead seads and logic to seed db, added script to json

---
+++ db/seedLeads.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import Lead from '../models/lead.js'
+import leads from './seedLeadsData.js'
+import { dbConnect, dbClose } from './connection.js'
+
+const seedLeads = async () => {
+    await Lead.deleteMany()
+    console.log('Leads deleted from DB')
+
+    await Lead.insertMany(leads)
+  console.log('Leads inserted to DB')
+}
+
+
+
+dbConnect()
+await seedLeads()
+dbClose()
+
