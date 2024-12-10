bf32e1706a0e3cca45d1fb6d3e7641f62056bd1c
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2024-12-10 13:07:55 +1100

    sorting functionality

--- routes/notes.js
+++ routes/notes.js
@@ -5,23 +5,34 @@ const router = express.Router();
 
 //   Middleware to handle retrieving assets by ID from DB
 const getNote = async (req, res, next) => {
-    let note;
-    try {
-      note = await Note.findById(req.params.id);
-      if (note == null) {
-        return res.status(404).json({ message: "Cannot find note" });
-      }
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+  let note;
+  try {
+    note = await Note.findById(req.params.id);
+    if (note == null) {
+      return res.status(404).json({ message: "Cannot find note" });
     }
-    res.note = note;
-    next();
-  }; 
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+  res.note = note;
+  next();
+};
 
 // Define routes individually
 router.get("/", async (req, res) => {
+  const query = req.query.sort;
   try {
-    const notes = await Note.find();
+    const sortObj =
+      req.query.sort == "due"
+        ? { due: 1 }
+        : query == "new"
+        ? { dateCreated: -1 }
+        : query == "old"
+        ? { dateCreated: 1 }
+        : null;
+    // console.log('params', req.query)
+    // const notes = await Note.find();
+    const notes = await Note.find().sort(sortObj);
     res.json(notes);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -30,7 +41,7 @@ router.get("/", async (req, res) => {
 // Find and countDocuments examples. Use param queries to implement?
 router.get("/count", async (req, res) => {
   try {
-    const notes = await  Note.countDocuments({createdBy: 'Big Seller'})
+    const notes = await Note.countDocuments({ createdBy: "Big Seller" });
     res.json(notes);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -54,32 +65,32 @@ router.post("/", async (req, res) => {
 router
   .route("/:id")
   .get(getNote, (req, res) => {
-    res.json(res.note)
+    res.json(res.note);
   })
   .patch(getNote, async (req, res) => {
-     if (req.body.title != null) {
-        res.note.title = req.body.title
-     }
-     if (req.body.body != null) {
-        res.note.body = req.body.body
-     }
-     if (req.body.due != null) {
-        res.note.due = req.body.due
-     }
-     try {
-        const updatedNote = await res.note.save();
-        res.json(updatedNote); 
-     } catch (err) {res.status(500).json({ message: err.message})
+    if (req.body.title != null) {
+      res.note.title = req.body.title;
     }
-})
+    if (req.body.body != null) {
+      res.note.body = req.body.body;
+    }
+    if (req.body.due != null) {
+      res.note.due = req.body.due;
+    }
+    try {
+      const updatedNote = await res.note.save();
+      res.json(updatedNote);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  })
   .delete(getNote, async (req, res) => {
     try {
-        await res.note.deleteOne();
-        res.json({ message: `Deleted note ${req.params.id}` });
+      await res.note.deleteOne();
+      res.json({ message: `Deleted note ${req.params.id}` });
     } catch (err) {
-        res.status(500).json({message: err.message})
+      res.status(500).json({ message: err.message });
     }
   });
-
 
 export default router;
