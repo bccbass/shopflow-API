fa107dbb7ac60abf58b987a4accd3eac97dfe88f
bccbass <109984363+bccbass@users.noreply.github.com>
2024-12-29 16:52:21 +1100

    updated archive model

--- models/Archive.js
+++ models/Archive.js
@@ -2,37 +2,37 @@ import mongoose from "mongoose";
 import followUpSchema from "./FollowUp.js";
 
 const archiveSchema = new mongoose.Schema({
-  dateCreated: {
-    required: true,
-    type: Date,
-    immutable: true,
-  },
-  nextContactDate: Date,
-  leadSource: String,
-  bookedTrial: Boolean,
-  enrolled: Boolean,
-  student: {
-    firstName: String,
-    lastName: String,
-    instrument: String,
-    groupClass: String,
-    age: Number,
-  },
-  guardian: {
-    firstName: String,
-    lastName: String,
-  },
-  contact: { phone: String, email: { type: String, lowercase: true } },
-  followUp: [followUpSchema],
-  trialLesson: {
-    date: Date,
-    time: String,
-    location: String,
-    instrument: String,
-    groupClass: String,
-    teacher: String,
-    followUp: [followUpSchema],
-  },
+	dateCreated: {
+		required: true,
+		type: Date,
+		immutable: true,
+	},
+	nextContactDate: Date,
+	leadSource: String,
+	bookedTrial: Boolean,
+	enrolled: Boolean,
+	student: {
+		firstName: String,
+		lastName: String,
+		instrument: String,
+		groupClass: String,
+		age: Number,
+	},
+	guardian: {
+		firstName: String,
+		lastName: String,
+	},
+	contact: { phone: String, email: { type: String, lowercase: true } },
+	followUp: [followUpSchema],
+	trialLesson: {
+		date: Date,
+		time: { hour: String, min: String, twelveHr: String },
+		location: String,
+		instrument: String,
+		groupClass: String,
+		teacher: String,
+		followUp: [followUpSchema],
+	},
 });
 
 export default mongoose.model("Archive", archiveSchema);
