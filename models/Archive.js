0fe21a795755dcc1a78cb3af40918439933f58cf
bccbass <109984363+bccbass@users.noreply.github.com>
2025-01-12 11:17:34 +1100

    updated Archive model to match Lead model

--- models/Archive.js
+++ models/Archive.js
@@ -1,41 +1,104 @@
 import mongoose from "mongoose";
 import followUpSchema from "./FollowUp.js";
+import { addDays, localDate, getDay } from "../lib/helperFuncs.js";
+
 
 const archiveSchema = new mongoose.Schema({
-	dateCreated: {
-		required: true,
-		type: Date,
-		immutable: true,
-		default: Date.now
-	},
-	nextContactDate: Date,
-	leadSource: String,
+    dateCreated: {
+      required: true,
+      type: Date,
+      default: Date.now,
+      immutable: true,
+    },
+    notes: String,
+    nextContactDate: { type: Date, default: addDays(2) },
+    leadSource: String,
     bookedTrial: { type: Boolean, default: false },
     enrolled: { type: Boolean, default: false },
-	student: {
-		firstName: String,
-		lastName: String,
-		instrument: String,
-		groupClass: String,
-		age: Number,
-	},
-	guardian: {
-		firstName: String,
-		lastName: String,
-	},
-	contact: { phone: String, email: { type: String, lowercase: true } },
-	followUp: [followUpSchema],
-	trialLesson: {
-		date: Date,
-		time: { hour: String, min: String, twelveHr: String },
-		location: String,
-		instrument: String,
-		groupClass: String,
-		teacher: String,
-		followUp: [followUpSchema],
-	},
-	notes: String
-	
-});
+    student: {
+      firstName: { type: String, trim: true },
+      lastName: { type: String, trim: true },
+      instrument: String,
+      groupClass: String,
+      age: Number,
+    },
+    guardian: {
+      firstName: { type: String, trim: true },
+      lastName: { type: String, trim: true },
+    },
+    contact: {
+      phone: { type: String, trim: true },
+      email: { type: String, lowercase: true, trim: true },
+    },
+    followUp: [followUpSchema],
+    correspondence: [],
+    trialLesson: {
+      date: Date,
+      time: { hour: String, min: String, twelveHr: String },
+      location: String,
+      instrument: String,
+      groupClass: String,
+      teacher: String,
+      followUp: [followUpSchema],
+    },
+  },
+  {
+    virtuals: {
+      studentFullName: {
+        get() {
+          return `${this.student.firstName} ${this.student.lastName}`;
+        },
+      },
+      guardianFullName: {
+        get() {
+          return `${this.guardian.firstName} ${this.guardian.lastName}`;
+        },
+      },
+      overdue: {
+        get() {
+          if (new Date(this.nextContactDate) < new Date("2020-01-01")) {
+            return false;
+          } else return new Date(this.nextContactDate) <= new Date(Date.now());
+        },
+      },
+      isMinor: {
+        get() {
+          return (
+            this.guardian.lastName.length > 0 ||
+            this.guardian.firstName.length > 0
+          );
+        },
+      },
+      trialDay: {
+        get() {
+          const dayDate = getDay(this.trialLesson.date)
+          return dayDate.split(',')[0]
+        },
+      },
+      trialDate: {
+        get() {
+          const dayDate = getDay(this.trialLesson.date)
+          return dayDate.split(' ')[2] + ' ' + dayDate.split(' ')[1]
+        },
+      },
+      contactDate: {
+        get() {
+          return localDate(this.nextContactDate);
+        },
+      },
+      createdDate: {
+        get() {
+          return localDate(this.dateCreated);
+        },
+      },
+      trialTime: {
+        get() {
+          return this.trialLesson?.time?.hour?.length == 0 ? '' : `${this.trialLesson?.time?.hour}:${this.trialLesson?.time?.min}${this?.trialLesson.time?.twelveHr}`;
+        },
+      },
+    },
+  }
+);
 
+archiveSchema.set("toJSON", { getters: true, virtuals: true });
 export default mongoose.model("Archive", archiveSchema);
